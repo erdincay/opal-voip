f9e49e7355034f17a32c1fb42f3c264f0661495a
csoutheren <csoutheren@023b2edf-31b2-4de3-b41e-bca80c47788f>
2007-12-08 06:50:42 +0000

    Fixed path problem

--- recipes/trunk/opal_bootstrap2.sh
+++ recipes/trunk/opal_bootstrap2.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 svn co https://opalvoip.svn.sourceforge.net/svnroot/opalvoip/ptlib/trunk ptlib
-svn co https://opalvoip.svn.sourceforge.net/svnroot/opalvoip/ptlib/opal opal
+svn co https://opalvoip.svn.sourceforge.net/svnroot/opalvoip/opal/trunk opal
